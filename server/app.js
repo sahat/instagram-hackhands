b9490041fe65d3f15794de16f37280535287233e
Sahat Yalkabov <sakhat@gmail.com>
2014-09-29 22:37:42 -0700

    fixed CORS route

--- server/app.js
+++ server/app.js
@@ -29,6 +29,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /*
  |--------------------------------------------------------------------------
+ | Enable CORS
+ |--------------------------------------------------------------------------
+ */
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  next();
+});
+
+/*
+ |--------------------------------------------------------------------------
  | Login Required Middleware
  |--------------------------------------------------------------------------
  */
