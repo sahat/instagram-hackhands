2c70e7436d1e38d87085b1a9fea7a8636b17d1ac
Sahat Yalkabov <sakhat@gmail.com>
2014-10-13 20:29:27 -0700

    query feed api only if logged in

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,12 +1,21 @@
 angular.module('Instagram')
   .controller('HomeCtrl', function($scope, $auth, API) {
 
+    if ($auth.isAuthenticated()) {
+      API.getFeed().success(function(data) {
+        $scope.photos = data;
+      });
+    }
+
     $scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
     };
 
-    API.getFeed().success(function(data) {
-      $scope.photos = data;
-    });
+    $scope.linkInstagram = function() {
+      $auth.link('instagram')
+        .then(function(response) {
+          console.log('done! linking');
+        });
+    };
 
   });
