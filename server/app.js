8d1269a95837758b40eef80e3c382ee3a98dfbd7
Sahat Yalkabov <sakhat@gmail.com>
2014-10-19 20:08:10 -0700

    code cleanup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var request = require('request');
-var jwt = require('jwt-simple');
-var mongoose = require('mongoose');
-var moment = require('moment');
-var cors = require('cors');
+var async = require('async');
 var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var path = require('path');
+var request = require('request');
 
 var config = require('./config');
 
@@ -27,7 +27,6 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(cors());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
