2606a24c2264deb5338bdb59ddbf0ac5be2ca0f5
Sahat Yalkabov <sakhat@gmail.com>
2014-12-07 20:28:42 -0800

    use process.env.db for database configuraation

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: 'localhost',
+  db: process.env.db || 'localhost',
   clientSecret: process.env.clientSecret || '46d6cc16b2fa445595e747a3349cb6e4',
   tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
 };
